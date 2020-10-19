e2338f311f6860a3678dc759adf4de6cea27c283
Zatyi94 <71665153+Zatyi94@users.noreply.github.com>
2020-10-19 21:14:44 +0200

    Add files via upload

--- programozasi_tetelek.py
+++ programozasi_tetelek.py
@@ -51,6 +51,73 @@ i = 0
 while i < n and x[i] != 2:
     i = i + 1
 if i < n:
-    print('van benne 2-es, ezen az indexen: {}'.format(i+1))
+    print('van benne 2-es, ezen az indexen: {}'.format(i))
 else:
-    print('nincs benne 2-es')     
+    print('nincs benne 2-es')
+
+# pythonban ha indexet keresünk:
+# hányadik elem a 2-es szám:
+print(x.index(2))
+
+# 4. megszámlálás tétele
+# számoljuk meg, hogy hány db páros szám van a listában
+x = [5, 7, 2, 9, 5, 4]
+db = 0
+for elem in x:
+    # oszthatóság vizsgálata maradékos osztás segítségével
+    # (páros szám esetében 2-vel)
+    if elem % 2 == 0:
+        db = db + 1
+print('{} db páros szám van a listában\n'.format(db))
+
+# 5. maximumkiválasztás tétele
+x = [5, 7, 2, 9, 5, 4]
+# van egy olyan függvény a pythonban, hogy max
+# ezt inkább ne írjuk felül, ezért legyen a neve max_index
+max_index = 0
+for i, num in enumerate(x):
+    if x[i] > x[max_index]:
+        max_index = i
+print("a legnagyobb elem a tömbben: {}".format(x[max_index]))
+
+# 6. buborékrendezés tétel
+# azért hívják buboréknak, mert az felfelé száll, és
+# a legagyobb elem is mindig felfelé kerül
+# rendezzük a tömb elemeit sorba
+x = [5, 7, 2, 9, 5, 4]
+n = len(x)
+for i in range(n - 1):
+    for j in range(0, n - i - 1):
+        if x[j] > x[j + 1]:
+            # kicseréljük a j-edik és a j+1-edik elemet:
+            x[j], x[j+1] = x[j+1], x[j]
+# a megrendezett x tömb:
+print(x)
+
+# az x tömb elemeit újra összekeverjük a következő példához
+x = [5, 7, 2, 9, 5, 4]
+
+# pythonban a lista rendezése:
+print(sorted(x))
+
+# 7. tétel: minimumkiválasztásos rendezés
+# itt lentről megyünk felfelé, előre rendezzük a kisebbeket
+# külső ciklus végig megy a tömb összes elemén, belső ciklus szűkül
+# hatékonyabb, mint a buborékrendezés
+# kevesebb cserét végez el (rögtön a helyére rakja az elemet)
+for i in range(0, n):
+    min_index = i
+    for j in range (i+1, n):
+        if x[min_index] > x[j]:
+            min_index = j
+    x[i], x[min_index] = x[min_index], x[i]
+print(x)
+
+# 8. tétel: faktoriális számítása
+def fakt(n):
+    if n == 0:
+        return 1
+    else: 
+        # rekurzív függvény: saját magát meghívja
+        return n * fakt(n-1)
+print(fakt(4))
